53708453d3c37a90ca0b04f28d43872b32d8f052
Alex <850946554@qq.com>
2016-05-13 14:40:21 +0800

    日常练习

--- src/main/lanqiaocup/algo/Main.java
+++ src/main/lanqiaocup/algo/Main.java
@@ -28,6 +28,7 @@ public class Main {
 		// ALGO_12();
 		// ALGO_11();
 		// ALGO_10();
+		// ALGO_9();
 		// ALGO_4();
 		// ALGO_3();
 		// ALGO_2();
@@ -929,6 +930,80 @@ public class Main {
 		}
 		return false;
 
+	}
+
+	// /*
+	// * 算法训练 摆动序列
+	// *
+	// * 问题描述
+	// * 如果一个序列满足下面的性质，我们就将它称为摆动序列：
+	// * 1. 序列中的所有数都是不大于k的正整数；
+	// * 2. 序列中至少有两个数。
+	// * 3. 序列中的数两两不相等；
+	// * 4. 如果第i – 1个数比第i – 2个数大，则第i个数比第i – 2个数小；如果
+	// * 第i – 1个数比第i – 2个数小，则第i个数比第i – 2个数大。
+	// * 比如，当k = 3时，有下面几个这样的序列：
+	// * 1 2
+	// * 1 3
+	// * 2 1
+	// * 2 1 3
+	// * 2 3
+	// * 2 3 1
+	// * 3 1
+	// * 3 2
+	// * 一共有8种，给定k，请求出满足上面要求的序列的个数。
+	// * 输入格式
+	// * 输入包含了一个整数k。（k<=20）
+	// * 输出格式
+	// * 输出一个整数，表示满足要求的序列个数。
+	// *
+	// * 样例输入
+	// * 3
+	// * 样例输出
+	// * 8
+	// */
+	@SuppressWarnings("unused")
+	private static void ALGO_9() {
+		Scanner sc = new Scanner(System.in);
+		ALGO_9_n = sc.nextInt();
+		sc.close();
+		for (int i = 0; i < ALGO_9_IsVisit.length; i++) {
+			ALGO_9_IsVisit[i] = false;
+		}
+		ALGO_9_DFS(1);
+		System.out.println(ALGO_9_ans);
+
+	}
+
+	private static int ALGO_9_n = 0;
+	private static int ALGO_9_ans = 0;
+	private static int ALGO_9_a[] = new int[25];
+	private static boolean ALGO_9_IsVisit[] = new boolean[25];
+
+	private static boolean ALGO_9_IsCan(int num, int k) {
+		if (k == 1 || k == 2)
+			return true;
+		if (ALGO_9_a[k - 1] > ALGO_9_a[k - 2] && num < ALGO_9_a[k - 2])
+			return true;
+		if (ALGO_9_a[k - 1] < ALGO_9_a[k - 2] && num > ALGO_9_a[k - 2])
+			return true;
+		return false;
+	}
+
+	private static void ALGO_9_DFS(int cur) {
+		if (cur > ALGO_9_n)
+			return;
+		for (int i = 1; i <= ALGO_9_n; i++) {
+			if (!ALGO_9_IsVisit[i] && ALGO_9_IsCan(i, cur)) {
+				ALGO_9_IsVisit[i] = true;
+				ALGO_9_a[cur] = i;
+				if (cur >= 2) {
+					ALGO_9_ans++;
+				}
+				ALGO_9_DFS(cur + 1);
+				ALGO_9_IsVisit[i] = false;
+			}
+		}
 	}
 
 	// /*
